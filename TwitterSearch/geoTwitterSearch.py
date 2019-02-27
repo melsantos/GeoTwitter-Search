bd74af8133d8e0b14b744f754d65632a309fe553
Melissa Santos <msant035@ucr.edu>
2019-02-26 22:15:35 -0800

    Titles are now added to Tweet Objects if applicable. Need to implement threading

--- TwitterSearch/geoTwitterSearch.py
+++ TwitterSearch/geoTwitterSearch.py
@@ -1,20 +1,21 @@
 from decouple import config
 from twitter.stream import TwitterStream, Timeout, HeartbeatTimeout, Hangup
 from twitter.oauth import OAuth
-import os, json, datetime
+from urllib import request
+from bs4 import BeautifulSoup
+import os, json, datetime, re
 
 consumer_key = config('CONSUMER_KEY')
 consumer_secret = config('CONSUMER_SECRET')
 token = config('ACCESS_TOKEN')
 token_secret =config('ACCESS_SECRET')
 
+cur_path = os.path.dirname(os.path.realpath(__file__))
+
 def main():
     start = datetime.datetime.now()
-
-    cur_path = os.path.dirname(os.path.realpath(__file__))
     tweet_folder = 'geo_tweets'
     tweet_out = open(os.path.join(cur_path, tweet_folder, "%s tweets.json" % start.strftime('%m-%d %H-%M')), 'w+')
-
     ts = TwitterStream(
         auth=OAuth(token, token_secret, consumer_key, consumer_secret))
     iterator = ts.statuses.sample()
@@ -24,19 +25,28 @@ def main():
             print("It's been an hour, bye!")
             break
         if tweet is Hangup:
-            print("Connection broken, bye!")
+            print("Connection broken at %s, bye!" % datetime.datetime.now().strftime('%H:%M'))
             break
         elif tweet is HeartbeatTimeout:
-            print("Heartbeat stopped, bye!")
+            print("Heartbeat stopped at %s, bye!" % datetime.datetime.now().strftime('%H:%M'))
             break
         elif tweet is Timeout:
-            print("Timed out, bye!")
+            print("Timed out at %s, bye!" % datetime.datetime.now().strftime('%H:%M'))
             break
         elif tweet is None:
             continue
         
         try:
             if tweet['coordinates']:
+                print('Coords founds!')
+                try:
+                    url = re.match(r'.*(http?s://.*).*', tweet['text']).group(1)
+                    content = request.urlopen(url).read()
+                    soup = BeautifulSoup(content).title.string
+                    tweet['title'] = soup
+                    print('URL found! Page Title: %s' % soup)
+                except:
+                    print("Could not open url")
                 json.dump(tweet, tweet_out)
                 tweet_out.write('\n')
         except:
